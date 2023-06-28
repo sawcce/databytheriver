01078591a91a576334428601d48e807149b9f5c6
alexis <sawcce@proton.me>
2023-06-28 20:10:31 +0200

    Fixed model struct visibility

--- lib/src/macros.rs
+++ lib/src/macros.rs
@@ -4,7 +4,7 @@ pub use dblib_macros::*;
 macro_rules! model {
     ($name:ident, $($field:ident:$type:tt),*) => {
         #[derive(Debug, Clone, Serialize, Deserialize, QueryParams)]
-        struct $name {
+        pub struct $name {
             id: dblib::RID,
             $($field: $type),*
         }
